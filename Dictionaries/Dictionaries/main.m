edd03cbe24a8e55fa5115c1d32c6ae950b0c76b2
Xiulan Shi <xiulanshi2013@gmail.com>
2016-03-14 13:33:16 -0400

    dictionary

---
+++ Dictionaries/Dictionaries/main.m
@@ -0,0 +1,42 @@
+//
+//  main.m
+//  Dictionaries
+//
+//  Created by Xiulan Shi on 3/14/16.
+//  Copyright © 2016 Xiulan Shi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    
+        NSMutableDictionary *glossary = [NSMutableDictionary dictionary];
+        
+        // Store three entries in the glossary
+        
+//        [glossary setObject:@"A class defined so other classes can inherit from it" forKey:@"abstract class"];
+//        [glossary setObject:@"To implement all the methods defined in a protocol" forKey:@"adopt"];
+//        [glossary setObject:@"Storing an object for later use" forKey:@"archiving"];
+        
+        glossary[@"abstract class"] = @"A class defined so other classes can inherit from it";
+        glossary[@"adopt"] = @"To implement all the methods defined in a protocol";
+        glossary[@"archiving"] = @"Storing an object for later use";
+        
+
+        
+        
+        // Retrieve and display them
+        
+//        NSLog(@"abstract class: %@", [glossary objectForKey:@"abstract class"]);
+//        NSLog(@"adopt: %@", [glossary objectForKey:@"adopt"]);
+//        NSLog(@"aechiving: %@", [glossary objectForKey:@"archiving"]);
+        
+        NSLog(@"abstract class: %@", glossary[@"abstract class"]);
+        NSLog(@"adopt: %@", glossary[@"adopt"]);
+        NSLog(@"aechiving: %@", glossary[@"archiving"]);
+       
+    }
+    return 0;
+}
