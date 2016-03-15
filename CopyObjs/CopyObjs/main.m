2f63bac0527ba685d8c1f97626de4f77e700fdad
Xiulan Shi <xiulanshi2013@gmail.com>
2016-03-15 13:26:43 -0400

    Deep Copying

--- CopyObjs/CopyObjs/main.m
+++ CopyObjs/CopyObjs/main.m
@@ -10,7 +10,7 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-       
+       /*
         NSMutableArray *dataArray = [NSMutableArray arrayWithObjects: @"one", @"two", @"three", @"four", nil];
         
         NSMutableArray *dataArray2;
@@ -46,6 +46,38 @@ int main(int argc, const char * argv[]) {
         for (NSString *elem in dataArray2) {
             NSLog(@"    %@", elem);
         }
+        */
+        
+        NSMutableArray *dataArray = [NSMutableArray arrayWithObjects:[NSMutableString stringWithString:@"one"], [NSMutableString stringWithString:@"two"], [NSMutableString stringWithString:@"three"], nil];
+        
+        NSMutableArray *dataArray2;
+        NSMutableString *mStr;
+        
+        NSLog(@"dataArray: ");
+        for (NSString *elem in dataArray) {
+            NSLog(@"    %@", elem);
+        }
+        
+        // make a copy, then change one of the strings
+        
+        dataArray2 = [dataArray mutableCopy];
+        
+        mStr = dataArray[0];
+        [mStr appendString: @"ONE"];
+        
+        NSLog(@"dataArray: ");
+        for (NSString *elem in dataArray) {
+            NSLog(@"    %@", elem);
+        }
+        
+        NSLog(@"dataArray2: ");
+        
+        for (NSString *elem in dataArray2) {
+            NSLog(@"    %@", elem);
+        }
+        
+        
+    
     }
     return 0;
 }
